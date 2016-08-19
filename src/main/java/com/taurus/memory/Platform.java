1176b19da9ec1b6b65047cac5eb76390afe62474
fengyannan <yn.feng@8win.com>
2016-08-19 15:04:31 +0800

    堆外内存分配 加入测试用例

--- src/main/java/com/taurus/memory/Platform.java
+++ src/main/java/com/taurus/memory/Platform.java
@@ -3,6 +3,8 @@ package com.taurus.memory;
 import sun.misc.Unsafe;
 
 import java.lang.reflect.Field;
+import java.nio.Buffer;
+import java.nio.ByteBuffer;
 
 /**
  * Created by ynfeng on 16/8/19.
@@ -18,6 +20,24 @@ public class Platform {
         } catch (Throwable cause) {
             unsafe = null;
         }
+    }
+
+    public static ByteBuffer allocateDirectBuffer(int size){
+        ByteBuffer buffer = ByteBuffer.allocate(size);
+        return buffer;
+    }
+
+    public static long directBufferAddress(ByteBuffer buffer){
+        Field field = null;
+        try {
+            field = Buffer.class.getDeclaredField("address");
+            field.setAccessible(true);
+            long fileOffset = unsafe.objectFieldOffset(field);
+            return unsafe.getLong(buffer,fileOffset);
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        }
+        return -1L;
     }
 
 
